35610ca39502f90db98471bd86103d1be0795837
pras1907 <pt15052000@gmail.com>
2023-07-05 23:50:44 +0530

    first commit-upto css adding

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+var items=[];
+
+app.set('view engine' , 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res){
+  var today  = new Date();
+  var options = { weekday: 'long', month: 'long', day: 'numeric' };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {kindOfDay:day, newListItems:items});
+});
+
+app.post("/", function(req, res){
+  var item =req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
+
+app.listen(3000, function(){
+  console.log("Server is started on port 3000...");
+});
