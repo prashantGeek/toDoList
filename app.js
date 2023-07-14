a523575d1c76775173cb78853affa40cd0844591
pras1907 <pt15052000@gmail.com>
2023-07-14 10:26:59 +0530

    till version 1

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app = express();
 var items=[];
+var workItems=[];
 
 app.set('view engine' , 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-
 app.get("/", function(req, res){
-  var today  = new Date();
-  var options = { weekday: 'long', month: 'long', day: 'numeric' };
-  var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay:day, newListItems:items});
+  let day =date.getDay();
+  res.render("list", {listTitle:day, newListItems:items});
 });
 
 app.post("/", function(req, res){
   var item =req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if(req.body.list==="Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+  }
+})
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work", newListItems:workItems});
+})
+
+app.get("/about", function(req,res){
+  res.render("about");
 })
 
 app.listen(3000, function(){
---
+++ date.js
@@ -0,0 +1,5 @@
+exports.getDay= function (){
+  var today  = new Date();
+  var options = { weekday: 'long', month: 'long', day: 'numeric' };
+  return today.toLocaleDateString("en-US", options);
+}
